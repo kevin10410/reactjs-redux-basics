cfd105c0662cb2c80f10d328da7d2abfbfdc3378
OTree <kevin10410@gmail.com>
2020-02-14 13:29:55 +0800

    refactor(store): add logger for middleware

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import mathReducer from './reducer/mathReducer';
 import userReducer from './reducer/userReducer';
 
+const logger = (store) => (next) => (action) => {
+  console.log('middlerware', action);
+  next(action);
+};
+
 const store = createStore(
   combineReducers({
     mathReducer,
     userReducer,
   }),
+  {},
+  applyMiddleware(logger),
 );
 
 store.subscribe(() => {
