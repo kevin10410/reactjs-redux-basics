610552681fe9ff670d718e34fbb7efcfb704113d
OTree <kevin10410@gmail.com>
2020-02-14 13:39:44 +0800

    refactor(store): replace logger with redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import mathReducer from './reducer/mathReducer';
 import userReducer from './reducer/userReducer';
-
-const logger = (store) => (next) => (action) => {
-  console.log('middlerware', action);
-  next(action);
-};
+import logger from 'redux-logger';
 
 const store = createStore(
   combineReducers({
     mathReducer,
     userReducer,
   }),
-  {},
   applyMiddleware(logger),
 );
-
-store.subscribe(() => {
-  console.log('store update', store.getState());
-});
 
 export default store;
